c5c284f161b5fc9422051233c200170b37888687
Matthew <mdaws89@gmail.com>
2020-09-29 15:13:29 -0500

    Module 1 code

---
+++ assignment1_mdawson.py
@@ -0,0 +1,26 @@
+#Module 1
+#Matt Dawson
+
+#integer
+integer = 2
+print(integer)
+
+#float
+float_ = 2.3
+print(float_)
+
+#boolean
+boolean = True
+print(boolean)
+
+#string
+string = "Hello"
+print(string)
+
+#tuple
+tuple_ = (1,2,3,4)
+print(tuple_)
+
+#dictionary
+dict_ = {"key1":1,"key2":2,"key3":3}
+print(dict_)
